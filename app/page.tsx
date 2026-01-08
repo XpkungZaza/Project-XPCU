a7c5e3e4fbed0aadaa183586938ed0c3f0e5aa76
XpkungZaza <you@example.com>
2026-01-08 09:33:34 +0700

    B
    completed

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
 
      
       <button className="mt-8 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
-        Start Reading
+        Start Reading now
       </button>
 
     </main>
